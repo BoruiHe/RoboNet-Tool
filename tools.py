b3310b4ef7943e27c368032dbac26118c9330c7f
BoruiHe <58924223+BoruiHe@users.noreply.github.com>
2021-08-02 16:08:21 -0400

    Add files via upload

--- tools.py
+++ tools.py
@@ -53,31 +53,72 @@ def weights_init(model):
         nn.init.normal_(model.weight.data, 1.0, 0.02)
         nn.init.constant_(model.bias.data, 0)
 
-class Generator(nn.Module):
+class Generator_s(nn.Module):
     def __init__(self, nz, ngf, nc):
-        super(Generator, self).__init__()
+        super(Generator_s, self).__init__()
         # add more ConTranspose2d layers.
         self.main = nn.Sequential(
             # input is Z, going into a convolution. Latent vector Z is of size(batch_size, 5, 1, 1)
             nn.ConvTranspose2d(nz, ngf * 8, kernel_size=(3, 4), stride=1, padding=0, bias=True),
             nn.BatchNorm2d(ngf * 8),
             nn.LeakyReLU(True),
-            # state size. (ngf*8) x 3 x 4 (batch_size, 512, 4, 4)
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, kernel_size=4, stride=2, padding=1, bias=True),
+            # state size. (ngf*8) x 4 x 3 (batch_size, 512, 4, 3)
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, kernel_size=6, stride=4, padding=1, bias=True),
             nn.BatchNorm2d(ngf * 4),
             nn.LeakyReLU(True),
-            # state size. (ngf*4) x 6 x 8
-            nn.ConvTranspose2d(ngf * 4, ngf * 2, kernel_size=4, stride=2, padding=1, bias=True),
+            # state size. (ngf*4) x 16 x 12
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, kernel_size=7, stride=5, padding=1, bias=True),
             nn.BatchNorm2d(ngf * 2),
             nn.LeakyReLU(True),
-            # state size. (ngf*2) x 12 x 16
-            nn.ConvTranspose2d(ngf * 2, ngf, kernel_size=4, stride=2, padding=1, bias=True),
+            # state size. (ngf*2) x 80 x 60
+            nn.ConvTranspose2d(ngf * 2, nc, kernel_size=6, stride=4, padding=1, bias=True),
+            # state size. (nc) x 320 x 240
+            nn.Tanh()
+        )
+    
+    def forward(self, input):
+        output = self.main(input)
+        return output
+
+class Generator(nn.Module):
+    def __init__(self, nz, ngf, nc):
+        super(Generator, self).__init__()
+        # add more ConTranspose2d layers.
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution. Latent vector Z is of size(batch_size, 5, 1, 1)
+            nn.ConvTranspose2d(nz, ngf * 64, kernel_size=(3, 4), stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(ngf * 64),
+            nn.LeakyReLU(True),
+            # state size. (ngf*8) x 3 x 4 (batch_size, 512, 3, 4)
+            nn.ConvTranspose2d(ngf * 64, ngf * 32, kernel_size=(3, 4), stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(ngf * 32),
+            nn.LeakyReLU(True),
+            # state size. (ngf*4) x 16 x 12 (batch_size, 256, 5, 7)
+            nn.ConvTranspose2d(ngf * 32, ngf * 16, kernel_size=4, stride=2, padding=0, bias=True),
+            nn.BatchNorm2d(ngf * 16),
+            nn.LeakyReLU(True),
+            # state size. (ngf*2) x 80 x 60 (batch_size, 128, 12, 16)
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, kernel_size=(3, 4), stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(ngf * 8),
+            nn.LeakyReLU(True),
+            # state size. (ngf) x 80 x 60 (batch_size, 64, 14, 19)
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, kernel_size=(3, 3), stride=2, padding=0, bias=True),
+            nn.BatchNorm2d(ngf * 4),
+            nn.LeakyReLU(True),
+            # state size. (ngf/2) x 80 x 60 (batch_size, 32, 29, 39)
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, kernel_size=(3, 3), stride=2, padding=0, bias=True),
+            nn.BatchNorm2d(ngf * 2),
+            nn.LeakyReLU(True),
+            # state size. (ngf/4) x 80 x 60 (batch_size, 16, 59, 79)
+            nn.ConvTranspose2d(ngf * 2, ngf, kernel_size=(3, 3), stride=2, padding=0, bias=True),
             nn.BatchNorm2d(ngf),
             nn.LeakyReLU(True),
-            # state size. (ngf) x 24 x 32
-            nn.ConvTranspose2d(ngf, nc, kernel_size=4, stride=2, padding=1, bias=True),
+            # state size. (ngf/8) x 80 x 60 (batch_size, 8, 119, 159)
+            nn.ConvTranspose2d(ngf, nc, kernel_size=(4, 4), stride=2, padding=0, bias=True),
+            nn.BatchNorm2d(nc),
+            nn.LeakyReLU(True),
+            # state size. (nc) x 240 x 320
             nn.Tanh()
-            # state size. (nc) x 48 x 64
         )
 
     def forward(self, input):
@@ -90,8 +131,7 @@ class My_loss(nn.Module):
         self.img_shape = img_shape
 
     def forward(self, fake, real):
-        assert tuple(fake.shape[1:]) == self.img_shape, 'images shape may be wrong'
-        assert real.permute(0,3,1,2).shape == fake.shape, 'fake images and real images got different shape'
-        real = real.permute(0,3,1,2)
-        loss = torch.sum((real-fake)**2)
+        assert fake.shape == self.img_shape, 'fakes shape may be wrong'
+        assert real.shape == fake.shape, 'fake images and real images got different shape'
+        loss = torch.sum((real-fake)**2)/torch.prod(torch.tensor(fake.shape))
         return loss
